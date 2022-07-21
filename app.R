37338eb7f5360de5116edaf8a1d7116cd4a94326
Philipp Thoss <ph.thoss@gmx.de>
2022-07-21 14:34:25 +0200

    asjust column layout and starting values to tesselation example

--- app.R
+++ app.R
@@ -19,13 +19,13 @@ get_plot_limits <- function(v) {
 }
 
 
-draw_voronoi_fermat_spiral <- function(from = 0, to = 111L, length = 333L) {
+draw_voronoi_fermat_spiral <- function(from = 0, to = 100L, length = 300L) {
   theta <- seq(from, to, length.out = length)
   x <- sqrt(theta) * cos(theta)
   y <- sqrt(theta) * sin(theta)
   pts <- cbind(x, y)
   opar <- par(
-    mar = c(0, 0, 0, 0), bg = "#696969"
+    mar = c(0, 0, 0, 0), bg = "black"
   )
 
   del <- delaunay(pts)
@@ -37,12 +37,18 @@ draw_voronoi_fermat_spiral <- function(from = 0, to = 111L, length = 333L) {
     xlab = NA, ylab = NA, axes = FALSE
   )
 
-  suppressMessages(plotVoronoiDiagram(v, colors = turbo(l)))
+  suppressMessages(
+    plotVoronoiDiagram(
+      v,
+      colors = turbo(l),
+      alpha = .5
+    )
+  )
 }
 
 ui <- semanticPage(
   tags$br(),
-  tags$head(tags$style("body {background-color: #696969; }")),
+  tags$head(tags$style("body {background-color: #000000; }")),
   div(
     class = "ui grid",
     div(
@@ -50,16 +56,17 @@ ui <- semanticPage(
       # input
       div(
         class = "column",
+        style = "width:10%!important",
         segment(
           form(
             field(
               numeric_input("from", "from", value = 0, min = 0, max = 1000)
             ),
             field(
-              numeric_input("to", "to", value = 111, min = 0, max = 1000)
+              numeric_input("to", "to", value = 100, min = 0, max = 1000)
             ),
             field(
-              numeric_input("length", "length", value = 333, min = 0, max = 1000)
+              numeric_input("length", "length", value = 300, min = 0, max = 1000)
             ),
             field(
               actionButton("goButton", "Go!", class = "btn-success")
@@ -70,7 +77,8 @@ ui <- semanticPage(
       # plot
       div(
         class = "column",
-        plotOutput("plot")
+        style = "width:90%!important",
+        plotOutput("plot", height = "900px")
       )
     )
   )
@@ -85,9 +93,7 @@ server <- function(input, output) {
   })
   output$plot <- renderPlot({
     if (is.null(v$from) & is.null(v$to) & is.null(v$flength)) {
-      return({
-        plot
-      })
+      return()
     }
     draw_voronoi_fermat_spiral(
       from = v$from,
